fc95515fb9a144e2a22a697fa5701eb35ccc9b89
victormenche <vistorme@gmail.com>
2018-11-17 23:15:37 +0100

    added home screen and FAB button

--- LonvaLops/src/app/app.module.ts
+++ LonvaLops/src/app/app.module.ts
@@ -2,15 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomMaterialModule } from './custom-material/custom-material.module';
+import { TitleViewComponent } from './title-view/title-view.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TitleViewComponent, AboutUsComponent],
+  imports: [BrowserModule, CustomMaterialModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ LonvaLops/src/app/title-view/title-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-title-view',
+  templateUrl: './title-view.component.html',
+  styleUrls: ['./title-view.component.scss']
+})
+export class TitleViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
