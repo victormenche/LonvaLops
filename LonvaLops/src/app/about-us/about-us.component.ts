28042f42cbb44f5a4e83b53fb2c0ceb16bdecb82
victormenche <vistorme@gmail.com>
2018-11-18 12:27:11 +0100

    Added custom SVG icons and chenged some styles

--- LonvaLops/src/app/about-us/about-us.component.ts
+++ LonvaLops/src/app/about-us/about-us.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.scss']
+  styleUrls: ['./about-us.component.scss'],
 })
 export class AboutUsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      `rat`,
+      this.domSanitizer.bypassSecurityTrustResourceUrl(`../../assets/rat.svg`),
+    );
+    this.matIconRegistry.addSvgIcon(
+      `minilop`,
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        `../../assets/bunny.svg`,
+      ),
+    );
   }
 
+  ngOnInit() {}
 }
--- LonvaLops/src/app/custom-material/custom-material.module.ts
+++ LonvaLops/src/app/custom-material/custom-material.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatToolbarModule, MatCardModule],
-  exports: [MatToolbarModule, MatCardModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
+    HttpClientModule,
+  ],
+  exports: [MatToolbarModule, MatCardModule, MatIconModule, HttpClientModule],
 })
 export class CustomMaterialModule {}
